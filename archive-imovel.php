de75d202912d8536824bf735df5b5023ebdc2e2c
Ingo Stramm <ingostramm@gmail.com>
2025-02-03 10:38:36 -0300

    fix footer in imovel archive

--- archive-imovel.php
+++ archive-imovel.php
@@ -35,8 +35,8 @@ get_header(); ?>
                     get_template_part('template-parts/content/content-none');
                 }
             }
-            mi_paging_nav();
-            get_footer(); ?>
+            mi_paging_nav(); ?>
         </div>
     </div>
 </div>
+<?php get_footer(); ?>
